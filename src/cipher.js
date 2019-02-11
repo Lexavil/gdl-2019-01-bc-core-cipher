1c6224915909f17a855fba96b23c087f3b5087c0
Lexavil <aviles.esp@gmail.com>
2019-02-11 16:53:38 -0600

    se encontro la falla de html

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,17 @@
 window.cipher = {
-  // ... 
+                   
+    decode: (string, offset) => { //metodo para codificar
+      let stringdecode = "";
+     
+      for (let i = 0; i < string.length; i++) { //Hace el recorrido por toda la longitus del string
+        if(string[i] === " "){ //Si hay un espacio, dejarlo igual
+          stringdecode += " ";
+              }else{
+                  let abc = (string.charCodeAt(i) + 65 - offset) % 26 + 65; //Formula magica
+                  stringdecode += (string.formCharCode(abc));
+                        }
+                    }
+                  return stringdecode ; //Nos retorna el resultado codificado
+        }
+        
 };
