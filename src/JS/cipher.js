a338a00909fd0d4de12071e8ee9417a3a5a5e15d
aviles.esp@gmail.com <aviles.esp@gmail.com>
2019-07-15 21:13:57 -0500

    identado a 4 espacios

--- src/JS/cipher.js
+++ src/JS/cipher.js
@@ -1,39 +1,37 @@
 window.cipher = {
   codificar: (offset, string) => {
-  let salida = '';
-  let oldAscii;
-  let newAscii;
-  let stringM = string.toUpperCase();
-  for (let i = 0; i < stringM.length; i++) {
-     oldAscii=stringM[i].charCodeAt();
-     if(oldAscii === 32){
-       newAscii = 32;
-     }
-      else {
-       newAscii = (oldAscii - 65 + offset) % 26 + 65;
-     }
-      salida = salida.concat(String.fromCharCode(newAscii));
-      
-  }
-    return salida;
+      let salida = '';
+      let oldAscii;
+      let newAscii;
+      let stringM = string.toUpperCase();
+      for (let i = 0; i < stringM.length; i++) {
+          oldAscii = stringM[i].charCodeAt();
+          if (oldAscii === 32) {
+              newAscii = 32;
+          } else {
+              newAscii = (oldAscii - 65 + offset) % 26 + 65;
+          }
+          salida = salida.concat(String.fromCharCode(newAscii));
+
+      }
+      return salida;
   },
-  
-    decodificar: (offset, string) => {
-    let salida = '';
-    let oldAscII;
-    let newAscII;
-    let stringM = string.toUpperCase();
-    for (let i = 0; i<stringM.length; i++){
-       oldAscII = stringM[i].charCodeAt();
-       if(oldAscII === 32){
-         newAscII = 32;
-       }
-        else {
-          newAscII = (oldAscII + 65 - offset) % 26 + 65;
-        }
-        salida = salida.concat(String.fromCharCode(newAscII));
-        
-    }
-     return salida;
+
+  decodificar: (offset, string) => {
+      let salida = '';
+      let oldAscII;
+      let newAscII;
+      let stringM = string.toUpperCase();
+      for (let i = 0; i < stringM.length; i++) {
+          oldAscII = stringM[i].charCodeAt();
+          if (oldAscII === 32) {
+              newAscII = 32;
+          } else {
+              newAscII = (oldAscII + 65 - offset) % 26 + 65;
+          }
+          salida = salida.concat(String.fromCharCode(newAscII));
+
+      }
+      return salida;
   }
-  };
+};
--- src/JS/index.js
+++ src/JS/index.js
@@ -1,35 +1,28 @@
-let codificar =() => {
+let codificar = () => {
     const string = document.getElementById("texto").value;
     const offset = parseInt(document.getElementById("offset").value);
-    document.getElementById('mensajeSecreto').innerHTML = window.cipher.codificar(offset,string);
-    document.getElementById('enco1').innerHTML =window.cipher.decodificar(offset,string);
-  
-  };
-  
-  let decodificar =() => {
+    document.getElementById('mensajeSecreto').innerHTML = window.cipher.codificar(offset, string);
+    document.getElementById('enco1').innerHTML = window.cipher.decodificar(offset, string);
+
+};
+
+let decodificar = () => {
     const string = document.getElementById("texto").value;
     const offset = parseInt(document.getElementById("offset").value);
-    document.getElementById('mensajeSecreto').innerHTML =window.cipher.decodificar(offset,string);
-    document.getElementById('enco1').innerHTML =window.cipher.decodificar(offset,string);
-  };
-  
-  //let demoVisibility =() => {
-  //  document.getElementById("enco").style.visibility = "hidden";
-  //};
-  
-  
-  let limpiar =() => {
-  document.getElementById("espacioMensaje").reset();
-  document.getElementById("clave").reset();
-  document.getElementById("mensajeSecreto").innerHTML= "";
-  //demoVisibility();
-  };
-  
-  //let limpiar = () => {
-  //  clear();
-  //  demoVisibility();
-  //}
-  
-  document.getElementById("codificar").addEventListener('click', codificar);
-  document.getElementById("decodificar").addEventListener('click', decodificar);
-  document.getElementById("limpiar").addEventListener('click', limpiar);
+    document.getElementById('mensajeSecreto').innerHTML = window.cipher.decodificar(offset, string);
+    document.getElementById('enco1').innerHTML = window.cipher.decodificar(offset, string);
+};
+
+
+
+let limpiar = () => {
+    document.getElementById("espacioMensaje").reset();
+    document.getElementById("clave").reset();
+    document.getElementById("mensajeSecreto").innerHTML = "";
+    
+};
+
+
+document.getElementById("codificar").addEventListener('click', codificar);
+document.getElementById("decodificar").addEventListener('click', decodificar);
+document.getElementById("limpiar").addEventListener('click', limpiar);
